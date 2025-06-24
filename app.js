99b85f7bda12a66f4c85bd3371979d2f14e57cac
Sumbul25 <92331578+Sumbul25@users.noreply.github.com>
2025-06-24 22:13:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+
+const socketIo = require('socket.io');
+
+const server = http.createServer(app);
+
+const io = socketIo(server);
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', function(socket)  {
+    socket.on("send-location", function(data) {
+        io.emit("receive-location", {id:socket.id, ...data});
+    });
+    socket.on("disconnect", function () {
+        io.emit("user-disconnected", socket.id);
+    }); 
+});
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+const PORT = process.env.PORT || 3000;
+server.listen(PORT);
